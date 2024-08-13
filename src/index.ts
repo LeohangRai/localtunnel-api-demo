71398f425bdd217eb280af4a8b183affec6baac2
Anup Rai <dev8@esignature.com.np>
2024-08-13 23:35:45 +0545

    feat: add express server; create a localtunnel inside of the express app listener

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-function greet() {
-  console.log(
-    `Hello World! The current date and time is ${new Date().toLocaleString()}`,
-  );
+import axios from 'axios';
+import 'dotenv/config';
+import express from 'express';
+import HttpStatus from 'http-status-codes';
+import localtunnel from 'localtunnel';
+
+const PORT = process.env.SERVER_PORT ?? 7777;
+const SUBDOMAIN = process.env.SUBDOMAIN;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  return res.json({
+    status: HttpStatus.OK,
+    message: 'Hello from Localtunnel!',
+  });
+});
+
+async function getMyPublicIp() {
+  try {
+    const response = await axios.get('https://loca.lt/mytunnelpassword');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching public IP:', error);
+  }
 }
 
-greet();
+app.listen(PORT, async () => {
+  console.info(`Server is up and running at http://localhost:${PORT}`);
+  try {
+    const tunnel = await localtunnel({
+      port: Number(PORT),
+      subdomain: SUBDOMAIN,
+    });
+    console.info(`Tunnel URL: ${tunnel.url}`);
+    const publicIp = await getMyPublicIp();
+    console.info(`Tunnel Password and Public IP:\n${publicIp}`);
+    tunnel.on('close', () => {
+      console.info('Tunnel closed');
+    });
+  } catch (error) {
+    console.error('Error creating tunnel:', error);
+  }
+});
